5332029139d2e6631ea1ddda9554387ade350160
rushil95 <46681749+rushil95@users.noreply.github.com>
2025-01-30 15:03:29 +0530

    Instance health status improvements (#516)
    * wip
    
    * Add row health status border color
    
    ---------
    
    Signed-off-by: rushil95 <46681749+rushil95@users.noreply.github.com>

--- app/(dashboard)/instances/[serviceId]/[servicePlanId]/[resourceId]/[instanceId]/[subscriptionId]/utils.ts
+++ app/(dashboard)/instances/[serviceId]/[servicePlanId]/[resourceId]/[instanceId]/[subscriptionId]/utils.ts
@@ -24,7 +24,7 @@ export const getTabs = (
     delete tabs.nodes;
   }
 
-  tabs["auditLogs"] = "Events";
+  tabs["auditLogs"] = "Activities";
   if (isBackup) {
     tabs["backups"] = "Backups";
   }
