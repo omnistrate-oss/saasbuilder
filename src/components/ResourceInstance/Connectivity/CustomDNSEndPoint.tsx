511d669678308b32b7b47e10430cd6549df0aa49
rushil95 <rushild95@gmail.com>
2025-02-04 16:03:49 +0530

    Remove unused import

--- src/components/ResourceInstance/Connectivity/CustomDNSEndPoint.tsx
+++ src/components/ResourceInstance/Connectivity/CustomDNSEndPoint.tsx
@@ -1,5 +1,4 @@
 import { Box, styled, SxProps, Theme } from "@mui/material";
-import Image from "next/image";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableRow from "@mui/material/TableRow";
