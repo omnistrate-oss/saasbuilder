fa0f839602ed44825d2f81455db79a3fa45c5cf2
mohandholu43 <mohandholu43@gmail.com>
2025-02-05 16:07:44 +0530

    fix

--- pages/access/service/[serviceId].js
+++ pages/access/service/[serviceId].js
@@ -791,8 +791,17 @@ function MarketplaceService() {
     },
     enableReinitialize: true,
     onSubmit: (values) => {
-      const data = cloneDeep(values);
       setIsCustomDNS(false);
+      const valuesCopy = cloneDeep(values);
+      const data = {};
+      //add non empty values to the payload
+      for (const key in valuesCopy) {
+        const value = valuesCopy[key];
+        if (value === undefined || value === "" || value === null) continue;
+        else {
+          data[key] = valuesCopy[key];
+        }
+      }
       async function getSchema() {
         try {
           let schemaArray = [];
