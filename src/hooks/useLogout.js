259c38abf1dc8e8c73672f8070595d27c6848b55
rushil95 <46681749+rushil95@users.noreply.github.com>
2025-02-07 20:55:59 +0530

    Revert "sso login change password send link for email (#532)"
    This reverts commit 734b5aa6ae1d4036d23ab317669cd7a932f7cc9f.

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -17,7 +17,6 @@ function useLogout() {
   function handleLogout() {
     Cookies.remove("token");
     localStorage.removeItem("paymentNotificationHidden");
-    localStorage.removeItem("loggedInUsingSSO");
     router.replace("/signin");
   }
 
