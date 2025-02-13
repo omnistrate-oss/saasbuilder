73459c2e20e8a68fd610745f941f752613deafe2
nihalmohammed18 <nihalm@omnistrate.com>
2025-02-13 11:24:26 +0530

    Fix

--- app/(dashboard)/billing/components/BillingCard.tsx
+++ app/(dashboard)/billing/components/BillingCard.tsx
@@ -10,7 +10,6 @@ const BillingCard: React.FC<BillingCardProps> = ({ title, icon, value }) => {
   return (
     <div className="flex flex-col items-center justify-center rounded-xl p-5 bg-white border border-[#E4E7EC] shadow-[0_1px_2px_0_#1018280D]">
       <div className="rounded-lg border border-[#E4E7EC] shadow-[0_1px_2px_0_#1018280D] h-12 w-12 flex items-center justify-center mb-3">
-        {/* TODO: There are more shadows to add here */}
         {icon}
       </div>
       <Text
