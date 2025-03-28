d6b5b04338794020e13169e2dab125fc78468025
NithishP <premprathipati1@gmail.com>
2025-03-28 16:25:35 +0530

    deprecate terraform

--- src/utils/constants/accountConfig.js
+++ src/utils/constants/accountConfig.js
@@ -1,4 +1,4 @@
-import { CLOUD_PROVIDERS } from "src/constants/cloudProviders";
+// import { CLOUD_PROVIDERS } from "src/constants/cloudProviders";
 
 export const ACCOUNT_CREATION_METHODS = {
   CLOUDFORMATION: "CloudFormation",
@@ -18,22 +18,22 @@ export const CLOUD_PROVIDER_DEFAULT_CREATION_METHOD = {
 };
 
 export const getAccountConfigStatusBasedHeader = (
-  status,
-  accountCreationMethod,
-  cloudprovider
+  status
+  // accountCreationMethod,
+  // cloudprovider
 ) => {
-  if (cloudprovider === CLOUD_PROVIDERS.gcp && !accountCreationMethod) {
-    if (status === "VERIFYING" || status === "PENDING") {
-      return "To complete the account configuration setup, choose one of the options provided below (GCP Cloud Shell or Terraform):";
-    }
+  // if (cloudprovider === CLOUD_PROVIDERS.gcp && !accountCreationMethod) {
+  //   if (status === "VERIFYING" || status === "PENDING") {
+  //     return "To complete the account configuration setup, choose one of the options provided below (GCP Cloud Shell or Terraform):";
+  //   }
 
-    if (status === "FAILED") {
-      return "The account configuration verification failed. Please review the instructions below to retry the setup using one of the options provided below (GCP Cloud Shell or Terraform) and resolve any issues:";
-    }
-    if (status === "READY") {
-      return "This account has already been configured successfully. However if you need to reconfigure for any reason, choose one of the options provided below (GCP Cloud Shell or Terraform):";
-    }
-  }
+  //   if (status === "FAILED") {
+  //     return "The account configuration verification failed. Please review the instructions below to retry the setup using one of the options provided below (GCP Cloud Shell or Terraform) and resolve any issues:";
+  //   }
+  //   if (status === "READY") {
+  //     return "This account has already been configured successfully. However if you need to reconfigure for any reason, choose one of the options provided below (GCP Cloud Shell or Terraform):";
+  //   }
+  // }
 
   if (status === "VERIFYING" || status === "PENDING") {
     return "To complete the account configuration setup:";
