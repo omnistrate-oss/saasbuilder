7bbba65012e41a02f33069bb5e6f0ac0ce69038d
rushil95 <rushild95@gmail.com>
2025-04-01 21:36:47 +0530

    wip

--- app/(dashboard)/billing/components/ConsumptionUsageChart.tsx
+++ app/(dashboard)/billing/components/ConsumptionUsageChart.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@mui/material";
 import dayjs from "dayjs";
 import React, { FC, useMemo } from "react";
 import {
@@ -18,231 +19,300 @@ function formatDate(inputDate) {
   return dayjs.utc(inputDate).format("MMM DD");
 }
 
-type ConsumptionUsageChartProps = {
-  usagePerDayData  : ConsumptionUsagePerDay ;
-  isFetchingUsagePerDay: boolean;
+export function handleYAxisShift(
+  chartID: string,
+  e: React.UIEvent<HTMLDivElement>
+) {
+  const allAxis = document.querySelectorAll(`#${chartID} .recharts-yAxis`);
+
+  const xAxis = document.querySelector(`#${chartID} .recharts-xAxis`);
+  const xAxisHeight = xAxis?.getBoundingClientRect().height || 0;
+
+  console.log("allAxis", allAxis);
+
+  allAxis?.forEach((axis) => {
+    const orientation =
+      axis
+        .querySelector(`.recharts-cartesian-axis-tick-line`)
+        ?.getAttribute("orientation") || "hor";
+
+    const rectElement = document.querySelector(
+      `#${chartID} .y-axis-rect-${orientation}`
+    );
+    if (!rectElement) {
+      //Adding a rect
+      const rect = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "rect"
+      );
+      const yAxisheight = axis.getBoundingClientRect().height;
+      const yAxisWidth = axis.getBoundingClientRect().width;
+
+      rect.setAttribute("x", "0");
+      rect.setAttribute("y", "0");
+      rect.setAttribute("width", `${yAxisWidth + 20}px`);
+      rect.setAttribute("height", `${yAxisheight + xAxisHeight}px`);
+      rect.setAttribute("fill", "white");
+      rect.setAttribute("class", `y-axis-rect-${orientation}`);
+
+      axis.insertBefore(rect, axis.firstChild);
+    }
+  });
+
+  const axis = document.querySelector(`#${chartID} .recharts-yAxis`);
+  //@ts-ignore
+  axis.style = "transform: translateX(" + e.target.scrollLeft + "px);"; //@ts-ignore
 }
 
- const ConsumptionUsageChart : FC<ConsumptionUsageChartProps> = (props) => {
-  const {usagePerDayData, isFetchingUsagePerDay} = props
+const chartMargins = { bottom: 50, top: 5, left: 0, right: 0 };
+const barCategoryGap = 50;
+const barWidth = 10;
 
-  const billingUsagePerDay = useMemo(() => {
+const scrollbarStyles = {
+  overflowX: "auto",
+  scrollbarWidth: "thin",
+  scrollbarColor: "#24273C",
+  "&::-webkit-scrollbar": {
+    width: "6px ",
+    height: "12px",
+  },
+  "&::-webkit-scrollbar-thumb": {
+    background: "#999a9f",
+    borderRadius: "6px",
+    border: "1px solid #999a9f",
+  },
+  "&::-webkit-scrollbar-track": {
+    background: " #24273C",
+    borderRadius: "6px",
+  },
+  "&::-webkit-scrollbar-thumb:hover": {
+    background: "#eaecf0",
+  },
+};
+
+type ConsumptionUsageChartProps = {
+  usagePerDayData: ConsumptionUsagePerDay | undefined;
+  isFetchingUsagePerDay: boolean;
+};
+
+const ConsumptionUsageChart: FC<ConsumptionUsageChartProps> = (props) => {
+  const { usagePerDayData, isFetchingUsagePerDay } = props;
+
+  const billingUsagePerDay: {
+    "Memory GiB hours": number;
+    "Storage GiB hours": number;
+    "CPU core hours": number;
+    date: string;
+  }[] = useMemo(() => {
     const usage = usagePerDayData?.usage || [];
-    const dataHashByDate = Record<string>
+    const dataHashByDate: Record<
+      string,
+      {
+        "Memory GiB hours": number;
+        "Storage GiB hours": number;
+        "CPU core hours": number;
+        date: string;
+      }
+    > = {};
 
-    usage.forEach((item) =>{})
+    console.log("usage", usage);
 
-    console.log("Usage per day data", usagePerDayData);
+    usage.forEach((usageDimensionData) => {
+      const { startTime: date, dimension, total: value } = usageDimensionData;
+      if (dataHashByDate[date as string]) {
+        dataHashByDate[date as string] = {
+          ...dataHashByDate[date as string],
+          [dimension as string]: value,
+        };
+      } else {
+        dataHashByDate[date as string] = {
+          "Memory GiB hours": 0,
+          "Storage GiB hours": 0,
+          "CPU core hours": 0,
+          [dimension as string]: value,
+          date: date as string,
+        };
+      }
+    });
+    return Object.values(dataHashByDate).sort((itemA, itemB) =>
+      itemA.date < itemB.date ? -1 : 1
+    );
   }, [usagePerDayData]);
+
+  const minChartWidth =
+    billingUsagePerDay.length * barWidth +
+    (billingUsagePerDay.length + 1) * barCategoryGap +
+    chartMargins.left +
+    chartMargins.right;
 
   console.log("billingUsagePerDay", billingUsagePerDay);
   const keys = ["Memory GiB hours", "Storage GiB hours", "CPU core hours"];
   const COLORS = ["#3E97FF", "#10AA50", "#7239EA"];
-  const translateX = [-17, -6, 5];
-
-  const minWidth = useMemo(() => {
-    if (billingUsagePerDay?.length < 10) {
-      return 800;
-    } else if (
-      billingUsagePerDay?.length > 9 &&
-      billingUsagePerDay?.length < 16
-    ) {
-      return 1200;
-    } else if (
-      billingUsagePerDay?.length > 15 &&
-      billingUsagePerDay?.length < 20
-    ) {
-      return 1600;
-    } else if (
-      billingUsagePerDay?.length > 19 &&
-      billingUsagePerDay?.length < 25
-    ) {
-      return 1800;
-    } else {
-      return 2300;
-    }
-  }, [billingUsagePerDay]);
+  const translateX = [-14, -3, 8];
 
   return isFetchingUsagePerDay ? (
     <LoadingSpinner />
   ) : (
-    <ReChartContainer
-      mt={2}
-      debounce={100}
-      height={370}
-      sx={{
-        overflowX: "auto",
-        scrollbarWidth: "thin",
-        scrollbarColor: "#24273C",
-        "&::-webkit-scrollbar": {
-          width: "6px ",
-          height: "12px",
-        },
-        "&::-webkit-scrollbar-thumb": {
-          background: "#999a9f",
-          borderRadius: "6px",
-          border: "1px solid #999a9f",
-        },
-        "&::-webkit-scrollbar-track": {
-          background: " #24273C",
-          borderRadius: "6px",
-        },
-        "&::-webkit-scrollbar-thumb:hover": {
-          background: "#eaecf0",
-        },
-      }}
-    >
-      <ResponsiveContainer height={350} minWidth={minWidth}>
-        <ComposedChart
-          data={billingUsagePerDay}
-          barCategoryGap={50} // Controls the space between groups of bars
-          barGap={1} // Ensures bars within the same group have no gap
-          margin={{ bottom: 50, top: 5 }}
+    <Box position="relative" id="usage-per-day-chart">
+      <ReChartContainer mt={2} debounce={100} height={370}>
+        <Box
+          sx={{overflowY: "hidden", ...scrollbarStyles }}
+          onScroll={(e: React.UIEvent<HTMLDivElement>) => {
+            handleYAxisShift("usage-per-day-chart", e);
+          }}
         >
-          <CartesianGrid strokeDasharray="5 5" vertical={false} />
-          <XAxis
-            dataKey="date"
-            tickLine={false}
-            axisLine={false}
-            interval={0}
-            tick={({ x, y, payload }) => (
-              <text
-                x={x}
-                y={y}
-                dy={15}
-                dx={21}
-                textAnchor="end"
-                style={{
-                  fontSize: 13,
-                  lineHeight: 14,
-                  fontWeight: 600,
-                  fill: "#7E8299",
-                }}
-              >
-                {formatDate(payload.value)}
-              </text>
-            )}
-          />
-
-          <YAxis
-            tickFormatter={(value) => `${value}`}
-            domain={([, datamax]) => [
-              0,
-              datamax > 0 ? Math.round(datamax + 1) : 1,
-            ]}
-            interval={0}
-            tick={({ x, y, payload }) => (
-              <text
-                x={x}
-                y={y}
-                dy={5}
-                textAnchor="end"
-                style={{
-                  fontSize: 13,
-                  lineHeight: 14,
-                  fontWeight: 600,
-                  fill: "#7E8299", // Apply color here
-                }}
-              >
-                {payload.value}
-              </text>
-            )}
-            tickLine={false}
-            axisLine={false}
-          />
-
-          <Tooltip
-            content={({ payload, label }) => {
-              if (!payload || payload.length === 0) return null;
-
-              // Define keys for line chart data
-              const lineKeys = new Set(); // Use a Set for better performance
-
-              // Filter only line data
-              const filteredData = payload.filter((entry) => {
-                const dataKey = String(entry.dataKey);
-                if (!lineKeys.has(dataKey)) {
-                  lineKeys.add(dataKey); // Add key to Set
-                  return true; // Keep this entry
-                }
-                return false; // Ignore duplicates
-              });
-
-              if (filteredData.length === 0) return null; // Don't show tooltip if no line data
-
-              return (
-                <div
-                  style={{
-                    background: "#fff",
-                    padding: "10px",
-                    borderRadius: "4px",
-                    boxShadow: "0px 0px 10px rgba(0,0,0,0.1)",
-                  }}
-                >
-                  <p
+          <ResponsiveContainer height={350} minWidth={minChartWidth}>
+            <ComposedChart
+              data={billingUsagePerDay}
+              barCategoryGap={50} // Controls the space between groups of bars
+              barGap={1} // Ensures bars within the same group have no gap
+              margin={chartMargins}
+            >
+              <CartesianGrid strokeDasharray="5 5" vertical={false} />
+              <XAxis
+                dataKey="date"
+                tickLine={false}
+                axisLine={false}
+                interval={0}
+                tick={({ x, y, payload }) => (
+                  <text
+                    x={x}
+                    y={y}
+                    dy={15}
+                    dx={21}
+                    textAnchor="end"
                     style={{
-                      margin: 0,
-                      fontWeight: "bold",
-                      fontSize: 14,
-                      color: "#7E8299",
+                      fontSize: 13,
+                      lineHeight: 14,
+                      fontWeight: 600,
+                      fill: "#7E8299",
                     }}
                   >
-                    {formatDate(label)}
-                  </p>
-                  {filteredData.map((entry) => (
-                    <p
-                      key={entry.dataKey}
+                    {formatDate(payload.value)}
+                  </text>
+                )}
+              />
+
+              <Tooltip
+                content={({ payload, label }) => {
+                  if (!payload || payload.length === 0) return null;
+
+                  // Define keys for line chart data
+                  const lineKeys = new Set(); // Use a Set for better performance
+
+                  // Filter only line data
+                  const filteredData = payload.filter((entry) => {
+                    const dataKey = String(entry.dataKey);
+                    if (!lineKeys.has(dataKey)) {
+                      lineKeys.add(dataKey); // Add key to Set
+                      return true; // Keep this entry
+                    }
+                    return false; // Ignore duplicates
+                  });
+
+                  if (filteredData.length === 0) return null; // Don't show tooltip if no line data
+
+                  return (
+                    <div
                       style={{
-                        color: entry.color,
-                        margin: 0,
-                        fontSize: 13,
-                        fontWeight: 400,
+                        background: "#fff",
+                        padding: "10px",
+                        borderRadius: "4px",
+                        boxShadow: "0px 0px 10px rgba(0,0,0,0.1)",
                       }}
                     >
-                      {entry.name}: {entry.value}
-                    </p>
-                  ))}
-                </div>
-              );
-            }}
-          />
+                      <p
+                        style={{
+                          margin: 0,
+                          fontWeight: "bold",
+                          fontSize: 14,
+                          color: "#7E8299",
+                        }}
+                      >
+                        {formatDate(label)}
+                      </p>
+                      {filteredData.map((entry) => (
+                        <p
+                          key={entry.dataKey}
+                          style={{
+                            color: entry.color,
+                            margin: 0,
+                            fontSize: 13,
+                            fontWeight: 400,
+                          }}
+                        >
+                          {entry.name}: {entry.value}
+                        </p>
+                      ))}
+                    </div>
+                  );
+                }}
+              />
 
-          {/* Dynamically Render Bars */}
-          {keys.map((key, index) => (
-            <Bar
-              key={`bar-${key}`}
-              dataKey={key}
-              barSize={10}
-              fill={COLORS[index % COLORS.length]}
-              radius={[4, 4, 4, 4]}
-            />
-          ))}
-
-          {/* Dynamically Render Lines with Active Dots */}
-          {keys.map((key, index) => (
-            <Line
-              id={`line-${key}`}
-              key={`line-${key}`}
-              type="monotone"
-              dataKey={key}
-              stroke={COLORS[index % COLORS.length]}
-              strokeDasharray="3 3"
-              dot={false}
-              activeDot={(props) => (
-                <circle
-                  {...props}
-                  transform={`translate(${translateX[index]}, 0)`}
-                  r={4}
+              {/* Dynamically Render Bars */}
+              {keys.map((key, index) => (
+                <Bar
+                  key={`bar-${key}`}
+                  dataKey={key}
+                  barSize={10}
                   fill={COLORS[index % COLORS.length]}
+                  radius={[4, 4, 4, 4]}
                 />
-              )}
-              transform={`translate(${translateX[index]}, 0)`}
-            />
-          ))}
-        </ComposedChart>
-      </ResponsiveContainer>
-    </ReChartContainer>
+              ))}
+
+              {/* Dynamically Render Lines with Active Dots */}
+              {keys.map((key, index) => (
+                <Line
+                  id={`line-${key}`}
+                  key={`line-${key}`}
+                  type="monotone"
+                  dataKey={key}
+                  stroke={COLORS[index % COLORS.length]}
+                  strokeDasharray="3 3"
+                  dot={false}
+                  activeDot={(props) => (
+                    <circle
+                      {...props}
+                      transform={`translate(${translateX[index]}, 0)`}
+                      r={4}
+                      fill={COLORS[index % COLORS.length]}
+                    />
+                  )}
+                  transform={`translate(${translateX[index]}, 0)`}
+                />
+              ))}
+              <YAxis
+                tickFormatter={(value) => `${value}`}
+                domain={([, datamax]) => [
+                  0,
+                  datamax > 0 ? Math.round(datamax + 1) : 1,
+                ]}
+                interval={0}
+                tick={({ x, y, payload }) => (
+                  <text
+                    x={x}
+                    y={y}
+                    dy={5}
+                    textAnchor="end"
+                    style={{
+                      fontSize: 13,
+                      lineHeight: 14,
+                      fontWeight: 600,
+                      fill: "#7E8299", // Apply color here
+                    }}
+                  >
+                    {payload.value}
+                  </text>
+                )}
+                tickLine={false}
+                axisLine={false}
+              />
+            </ComposedChart>
+          </ResponsiveContainer>
+        </Box>
+      </ReChartContainer>
+    </Box>
   );
-}
+};
 
 export default ConsumptionUsageChart;
