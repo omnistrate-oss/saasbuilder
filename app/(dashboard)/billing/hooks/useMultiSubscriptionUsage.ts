a8d2b037832fcf9d66de4440496913e5143a5454
rushil95 <46681749+rushil95@users.noreply.github.com>
2025-04-16 13:57:41 +0530

    Subscription based usage breakdown (#619)
    * Subscription based usage breakdown
    
    * PR feeback changes

---
+++ app/(dashboard)/billing/hooks/useMultiSubscriptionUsage.ts
@@ -0,0 +1,60 @@
+import { useQuery } from "@tanstack/react-query";
+import { getConsumptionUsage } from "src/api/consumption";
+
+function useMultiSubscriptionUsage(queryParams: { subscriptionIds: string[] }) {
+  const { subscriptionIds = [] } = queryParams;
+
+  const enabled = subscriptionIds.length > 0;
+
+  const query = useQuery({
+    queryKey: ["multi-subscription-consumption", subscriptionIds],
+    queryFn: async () => {
+      const subscriptionUsageDataMap: Record<
+        string,
+        {
+          storageGiBHours: number;
+          memoryGiBHours: number;
+          cpuCoreHours: number;
+        }
+      > = {};
+
+      await Promise.all(
+        subscriptionIds.map((subscriptionId) =>
+          getConsumptionUsage({ subscriptionID: subscriptionId }).then(
+            (response) => {
+              const usage = response.data.usage || [];
+
+              const usageData = {
+                storageGiBHours: 0,
+                memoryGiBHours: 0,
+                cpuCoreHours: 0,
+              };
+              usage.forEach((usageDatapoint) => {
+                //aggregate storage cpu and memory data points
+                const { dimension, total } = usageDatapoint;
+                if (total) {
+                  if (dimension === "Memory GiB hours") {
+                    usageData.memoryGiBHours = total;
+                  } else if (dimension === "Storage GiB hours") {
+                    usageData.storageGiBHours = total;
+                  } else if (dimension === "CPU core hours") {
+                    usageData.cpuCoreHours = total;
+                  }
+                }
+              });
+
+              subscriptionUsageDataMap[subscriptionId] = usageData;
+            }
+          )
+        )
+      );
+
+      return subscriptionUsageDataMap;
+    },
+    enabled: enabled,
+  });
+
+  return query;
+}
+
+export default useMultiSubscriptionUsage;
