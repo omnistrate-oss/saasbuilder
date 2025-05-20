cb752e4854c1db901231900f0a473f957eeb891b
mohandholu43 <mohandholu43@gmail.com>
2025-05-20 17:06:12 +0530

    unuser

--- components/ui/chart.tsx
+++ components/ui/chart.tsx
@@ -112,7 +112,6 @@ const ChartTooltipContent = React.forwardRef<
       label,
       labelFormatter,
       labelClassName,
-      formatter,
       color,
       nameKey,
       labelKey,
@@ -165,7 +164,7 @@ const ChartTooltipContent = React.forwardRef<
         {!nestLabel ? tooltipLabel : null}
 
         <div className="grid gap-1.5">
-          {payload.map((item, index) => {
+          {payload.map((item) => {
             const key = `${nameKey || item.name || item.dataKey || "value"}`;
             const itemConfig = getPayloadConfigFromPayload(config, item, key);
             const indicatorColor = color || item.payload.fill || item.color;
